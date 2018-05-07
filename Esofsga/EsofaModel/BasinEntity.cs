45a581b351b269763827af3bae5e993f440250d2
iCoderWang <wangmouren_icoder@qq.com>
2018-05-08 01:21:16 +0800

    Some Entities were created for Basin (Prospective Area), Block (Favorable Area) and Target (Core Area) on May, 8th, 2018

---
+++ Esofsga/EsofaModel/BasinEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsofaModel
+{
+    //远景区数据实体 (Basin: Prosepctive Area)
+    public partial class BasinEntity
+    {
+        //远景区实体所有参数用 bsn_ 开始
+    }
+}
---
+++ Esofsga/EsofaModel/BlockEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsofaModel
+{
+    //有利区数据实体（Block: Favorit Area）
+    public partial class BlockEntity
+    {
+        //有利区实体所有参数用 blk_ 开始
+    }
+}
