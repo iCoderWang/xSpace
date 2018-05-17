78e9c7d24710108304daaf1bc0d3023a68effb64
iCoderWang <wangmouren_icoder@qq.com>
2018-05-17 22:53:59 +0800

    EsofaCommon/DataAssignment.cs was modified : the Method Assign were overriding for BasinEntity, BlockEntity and TargetEntity on 2018-5-17 PM 22:53.

--- Esofsga/EsofaCommon/DataAssignment.cs
+++ Esofsga/EsofaCommon/DataAssignment.cs
@@ -6,6 +6,7 @@ namespace EsofaCommon
 {
     public partial class DataAssignment
     {
+        //RawData数据体 实现数据表格化分配的方法 Asssign
         public List<RawData> Assign (List<RawData> list, DataTable dt)
         {
             foreach (DataRow row in dt.Rows)
@@ -68,6 +69,252 @@ namespace EsofaCommon
 
                     //获取字段 地质资源量 (万亿方) geological resource
                     para_Gr = row[18].ToString()
+                });
+
+            }
+            //将集合返回
+            return list;
+        }
+
+        //远景区 BasinEntity数据体 实现数据表格化分配方法 Assign 重载
+        public List<BasinEntity> Assign(List<BasinEntity> list, DataTable dt)
+        {
+            foreach (DataRow row in dt.Rows)
+            {
+                list.Add(new BasinEntity()
+                {
+                    //获取字段 盆地名字 basin name
+                    bsn_Att_Name = row[0].ToString(),
+
+                    //获取字段 盆地代码 basin code
+                    // bsn_Att_Code = row[0].ToString(),
+
+                    //获取字段 主力层系 primary Strata
+                    bsn_Att_Ps = row[1].ToString(),
+
+                    //定义字段 保存条件 Storage Condition
+                    bsn_Att_Para_Sc = row[2].ToString(),
+
+                    //定义字段 地质资源量 (万亿方) geological resource
+                    bsn_Att_Para_Gr = row[3].ToString(),
+
+                    //定义字段 富有机质页岩厚度范围(m) Thinckness Range Riching Organic Matters Shale
+                    bsn_Geo_Para_TrRoms = row[4].ToString(),
+
+                    //定义字段 Toc 总有机碳
+                    bsn_Geo_Para_Toc = row[5].ToString(),
+
+                    //定义字段 Kt 有机质类型 Kerogen Type
+                    bsn_Geo_Para_Kt = row[6].ToString(),
+
+                    //定义字段 Ro 有机质成熟度
+                    bsn_Geo_Para_Ro = row[7].ToString(),
+
+                    //定义字段 有效圈定面积(km2) Effective Area
+                    bsn_Geo_Para_Ea = row[8].ToString(),
+
+                    //定义字段 资源丰度 (108 m3/km2) Resource richness
+                    bsn_Geo_Para_Rr = row[9].ToString(),
+
+                    //定义字段 构造复杂程度 Structure Complexity degree
+                    bsn_Geo_Para_Scd = row[10].ToString(),
+
+                    //定义字段 埋深范围(m)  Depth Range 
+                    bsn_Eng_Para_Dr = row[11].ToString(),
+
+                    //定义字段 脆性矿物含量 Brittle Mineral Content
+                    bsn_Eng_Para_Bmc = row[12].ToString()
+                });
+
+            }
+            //将集合返回
+            return list;
+        }
+
+        //有利区 BlockEntity数据体 实现数据表格化分配方法 Assign 重载
+        public List<BlockEntity> Assign(List<BlockEntity> list, DataTable dt)
+        {
+            foreach (DataRow row in dt.Rows)
+            {
+                list.Add(new BlockEntity()
+                {
+                    //获取字段 盆地名字 basin name
+                    bsn_Att_Name = row[0].ToString(),
+
+                    //获取字段 盆地代码 basin code
+                    // bsn_Att_Code = row[0].ToString(),
+
+                    //获取字段 区块名字 block name
+                    blk_Att_Name = row[1].ToString(),
+
+                    //获取字段 区块代码 block code
+                    // blk_Att_Code = row[0].ToString(),
+
+                    //获取字段 主力层系 primary Strata
+                    blk_Att_Ps = row[2].ToString(),
+
+                    //定义字段 保存条件 Storage Condition
+                    blk_Att_Para_Sc = row[3].ToString(),
+
+                    //定义字段 地质资源量 (万亿方) geological resource
+                    blk_Att_Para_Gr = row[4].ToString(),
+
+                    //定义字段 富有机质页岩厚度范围(m) Thinckness Range Riching Organic Matters Shale
+                    blk_Geo_Para_TrRoms = row[5].ToString(),
+
+                    //定义字段 Toc 总有机碳
+                    blk_Geo_Para_Toc = row[6].ToString(),
+
+                    //定义字段 Kt 有机质类型 Kerogen Type
+                    blk_Geo_Para_Kt = row[7].ToString(),
+
+                    //定义字段 Ro 有机质成熟度
+                    blk_Geo_Para_Ro = row[8].ToString(),
+
+                    //定义字段 有效圈定面积(km2) Effective Area
+                    blk_Geo_Para_Ea = row[9].ToString(),
+
+                    //定义字段 含气量 (m3/t) Gas Content
+                    blk_Geo_Para_Gc = row[10].ToString(),
+
+                    //定义字段 资源丰度 (108 m3/km2) Resource richness
+                    blk_Geo_Para_Rr = row[11].ToString(),
+
+                    //定义字段 孔隙度 (%) Porosity
+                    blk_Geo_Para_Por = row[12].ToString(),
+
+                    //定义字段 构造复杂程度 Structure Complexity degree
+                    blk_Geo_Para_Scd = row[13].ToString(),
+
+                    //定义字段 埋深范围(m)  Depth Range 
+                    blk_Eng_Para_Dr = row[14].ToString(),
+
+                    //定义字段 压力系数 Pressure Coefficient (Factor)
+                    blk_Eng_Para_Pc = row[15].ToString(),
+
+                    //定义字段 脆性矿物含量 Brittle Mineral Content
+                    blk_Eng_Para_Bmc = row[16].ToString(),
+
+                    //定义字段 地表地貌 Suface and Geography
+                    blk_Mkt_Para_Sg = row[17].ToString()
+                });
+
+            }
+            //将集合返回
+            return list;
+        }
+
+        //核心区 TargetEntity数据体 实现数据表格化分配方法 Assign 重载
+        public List<TargetEntity> Assign(List<TargetEntity> list, DataTable dt)
+        {
+            foreach (DataRow row in dt.Rows)
+            {
+                list.Add(new TargetEntity()
+                {
+                    //获取字段 盆地名字 basin name
+                    bsn_Att_Name = row[0].ToString(),
+
+                    //获取字段 盆地代码 basin code
+                    // bsn_Att_Code = row[0].ToString(),
+
+                    //获取字段 区块名字 block name
+                    blk_Att_Name = row[1].ToString(),
+
+                    //获取字段 区块代码 block code
+                    // blk_Att_Code = row[0].ToString(),
+
+                    //获取字段 目标区名字 Target name
+                    tgt_Att_Name = row[2].ToString(),
+
+                    //获取字段 目标区代码 target code
+                    // tgt_Att_Code = row[0].ToString(),
+
+                    //获取字段 主力层系 primary Strata
+                    tgt_Att_Ps = row[3].ToString(),
+
+                    //定义字段 保存条件 Storage Condition
+                    tgt_Att_Para_Sc = row[4].ToString(),
+
+                    //定义字段 地质资源量 (万亿方) geological resource
+                    tgt_Att_Para_Gr = row[5].ToString(),
+
+                    //定义字段 富有机质页岩厚度范围(m) Thinckness Range Riching Organic Matters Shale
+                    tgt_Geo_Para_TrRoms = row[6].ToString(),
+
+                    //定义字段 Toc 总有机碳
+                    tgt_Geo_Para_Toc = row[7].ToString(),
+
+                    //定义字段 Kt 有机质类型 Kerogen Type
+                    tgt_Geo_Para_Kt = row[8].ToString(),
+
+                    //定义字段 Ro 有机质成熟度
+                    tgt_Geo_Para_Ro = row[9].ToString(),
+
+                    //定义字段 有效圈定面积(km2) Effective Area
+                    tgt_Geo_Para_Ea = row[10].ToString(),
+
+                    //定义字段 含气量 (m3/t) Gas Content
+                    tgt_Geo_Para_Gc = row[11].ToString(),
+
+                    //定义字段 资源丰度 (108 m3/km2) Resource richness
+                    tgt_Geo_Para_Rr = row[12].ToString(),
+
+                    //定义字段 孔隙度 (%) Porosity
+                    tgt_Geo_Para_Por = row[13].ToString(),
+
+                    //定义字段 构造复杂程度 Structure Complexity degree
+                    tgt_Geo_Para_Scd = row[14].ToString(),
+
+                    //定义字段 顶板岩性 Roof Lithology
+                    tgt_Geo_Para_Rfl = row[15].ToString(),
+
+                    //定义字段 顶板厚度 Roof Thickness
+                    tgt_Geo_Para_Rft = row[16].ToString(),
+
+                    //定义字段 底板岩性 Floor Lithology
+                    tgt_Geo_Para_Frl = row[17].ToString(),
+
+                    //定义字段 底板厚度 Floor Thickness
+                    tgt_Geo_Para_Frt = row[18].ToString(),
+
+                    //定义字段 埋深范围(m)  Depth Range 
+                    tgt_Eng_Para_Dr = row[19].ToString(),
+
+                    //定义字段 压力系数 Pressure Coefficient (Factor)
+                    tgt_Eng_Para_Pc = row[20].ToString(),
+
+                    //定义字段 渗透率 Permeability
+                    tgt_Eng_Para_Per = row[21].ToString(),
+
+                    //定义字段 裂缝发育程度 Fracture Development Degree
+                    tgt_Eng_Para_Fdd = row[22].ToString(),
+
+                    //定义字段 主应力差异系数 Principle Stress Diversity Coefficient (Factor)
+                    tgt_Eng_Para_Psdc = row[23].ToString(),
+
+                    //定义字段 脆性矿物含量 Brittle Mineral Content
+                    tgt_Eng_Para_Bmc = row[24].ToString(),
+
+                    //定义字段 水系 Drainage System
+                    tgt_Eng_Para_Ds = row[25].ToString(),
+
+                    //定义字段 区域勘探程度 Local Exploring Degree
+                    tgt_Eng_Para_Led = row[26].ToString(),
+
+                    //定义字段 市场气价 Gas Price
+                    tgt_Mkt_Para_Gp = row[27].ToString(),
+
+                    //定义字段 市场需求 Demand
+                    tgt_Mkt_Para_Dmd = row[28].ToString(),
+
+                    //定义字段 交通设施 Transport Utility
+                    tgt_Mkt_Para_Tu = row[29].ToString(),
+
+                    //定义字段 管网条件 Pipe Net
+                    tgt_Mkt_Para_Pn = row[30].ToString(),
+
+                    //定义字段 地表地貌 Suface and Geography
+                    tgt_Mkt_Para_Sg = row[31].ToString()
                 });
 
             }
--- Esofsga/EsofaUI/ImportingRawDataFrm.Designer.cs
+++ Esofsga/EsofaUI/ImportingRawDataFrm.Designer.cs
@@ -34,13 +34,17 @@
             this.btnPreview = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // btnBrowser
             // 
-            this.btnBrowser.Location = new System.Drawing.Point(720, 40);
+            this.btnBrowser.Location = new System.Drawing.Point(712, 30);
             this.btnBrowser.Name = "btnBrowser";
-            this.btnBrowser.Size = new System.Drawing.Size(57, 23);
+            this.btnBrowser.Size = new System.Drawing.Size(65, 25);
             this.btnBrowser.TabIndex = 0;
             this.btnBrowser.Text = "浏览";
             this.btnBrowser.UseVisualStyleBackColor = true;
@@ -49,7 +53,7 @@
             // lblFile
             // 
             this.lblFile.AutoSize = true;
-            this.lblFile.Location = new System.Drawing.Point(23, 47);
+            this.lblFile.Location = new System.Drawing.Point(23, 37);
             this.lblFile.Name = "lblFile";
             this.lblFile.Size = new System.Drawing.Size(43, 13);
             this.lblFile.TabIndex = 1;
@@ -57,16 +61,16 @@
             // 
             // txtSourceDataPath
             // 
-            this.txtSourceDataPath.Location = new System.Drawing.Point(64, 43);
+            this.txtSourceDataPath.Location = new System.Drawing.Point(64, 33);
             this.txtSourceDataPath.Name = "txtSourceDataPath";
-            this.txtSourceDataPath.Size = new System.Drawing.Size(637, 20);
+            this.txtSourceDataPath.Size = new System.Drawing.Size(619, 20);
             this.txtSourceDataPath.TabIndex = 2;
             // 
             // btnPreview
             // 
-            this.btnPreview.Location = new System.Drawing.Point(542, 102);
+            this.btnPreview.Location = new System.Drawing.Point(618, 93);
             this.btnPreview.Name = "btnPreview";
-            this.btnPreview.Size = new System.Drawing.Size(74, 26);
+            this.btnPreview.Size = new System.Drawing.Size(65, 25);
             this.btnPreview.TabIndex = 3;
             this.btnPreview.Text = "预览";
             this.btnPreview.UseVisualStyleBackColor = true;
@@ -74,7 +78,7 @@
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(636, 102);
+            this.btnCancel.Location = new System.Drawing.Point(712, 93);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(65, 25);
             this.btnCancel.TabIndex = 4;
@@ -82,11 +86,56 @@
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(67, 70);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(61, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "远景区";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "区域：";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(146, 70);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.Text = "有利区";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(231, 70);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(61, 17);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.Text = "目标区";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
             // ImportingRawDataFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(849, 152);
+            this.ClientSize = new System.Drawing.Size(804, 140);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnPreview);
             this.Controls.Add(this.txtSourceDataPath);
@@ -109,5 +158,9 @@
         private System.Windows.Forms.Button btnPreview;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
     }
 }
