6794fc761410a5735534b1d1d36657e30460c2bb
iCoderWang <wangmouren_icoder@qq.com>
2018-05-28 02:57:56 +0800

    GradingFrm was updated with Basin,Block and Target tab pages in the Frm on 2018-5-28

--- Esofsga/EsofaUI/GradingFrm.Designer.cs
+++ Esofsga/EsofaUI/GradingFrm.Designer.cs
@@ -84,10 +84,16 @@
             this.splitCtnerGraFrm = new System.Windows.Forms.SplitContainer();
             this.treeViewGrad = new System.Windows.Forms.TreeView();
             this.splitContDataZone = new System.Windows.Forms.SplitContainer();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnNext = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControlGrading = new System.Windows.Forms.TabControl();
+            this.tabPageBasin = new System.Windows.Forms.TabPage();
+            this.tabPageBlock = new System.Windows.Forms.TabPage();
+            this.tabPageTarget = new System.Windows.Forms.TabPage();
+            this.dgvBasin = new System.Windows.Forms.DataGridView();
+            this.dgvBlock = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.splitCtnerGraFrm)).BeginInit();
             this.splitCtnerGraFrm.Panel1.SuspendLayout();
             this.splitCtnerGraFrm.Panel2.SuspendLayout();
@@ -96,8 +102,14 @@
             this.splitContDataZone.Panel1.SuspendLayout();
             this.splitContDataZone.Panel2.SuspendLayout();
             this.splitContDataZone.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
+            this.tabControlGrading.SuspendLayout();
+            this.tabPageBasin.SuspendLayout();
+            this.tabPageBlock.SuspendLayout();
+            this.tabPageTarget.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBasin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBlock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // splitCtnerGraFrm
@@ -117,8 +129,8 @@
             this.splitCtnerGraFrm.Panel2.Controls.Add(this.splitContDataZone);
             this.splitCtnerGraFrm.Panel2.Padding = new System.Windows.Forms.Padding(2);
             this.splitCtnerGraFrm.Panel2MinSize = 900;
-            this.splitCtnerGraFrm.Size = new System.Drawing.Size(1178, 555);
-            this.splitCtnerGraFrm.SplitterDistance = 186;
+            this.splitCtnerGraFrm.Size = new System.Drawing.Size(1388, 636);
+            this.splitCtnerGraFrm.SplitterDistance = 219;
             this.splitCtnerGraFrm.TabIndex = 1;
             // 
             // treeViewGrad
@@ -183,11 +195,12 @@
             treeNode54.Text = "盆地";
             this.treeViewGrad.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
             treeNode54});
-            this.treeViewGrad.Size = new System.Drawing.Size(184, 553);
+            this.treeViewGrad.Size = new System.Drawing.Size(217, 634);
             this.treeViewGrad.TabIndex = 0;
             // 
             // splitContDataZone
             // 
+            this.splitContDataZone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.splitContDataZone.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContDataZone.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
             this.splitContDataZone.IsSplitterFixed = true;
@@ -197,32 +210,23 @@
             // 
             // splitContDataZone.Panel1
             // 
-            this.splitContDataZone.Panel1.Controls.Add(this.dataGridView1);
+            this.splitContDataZone.Panel1.Controls.Add(this.tabControlGrading);
             // 
             // splitContDataZone.Panel2
             // 
             this.splitContDataZone.Panel2.Controls.Add(this.btnCancel);
             this.splitContDataZone.Panel2.Controls.Add(this.btnNext);
-            this.splitContDataZone.Size = new System.Drawing.Size(982, 549);
-            this.splitContDataZone.SplitterDistance = 498;
+            this.splitContDataZone.Size = new System.Drawing.Size(1161, 632);
+            this.splitContDataZone.SplitterDistance = 581;
             this.splitContDataZone.TabIndex = 3;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(982, 498);
-            this.dataGridView1.TabIndex = 0;
             // 
             // btnCancel
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCancel.Location = new System.Drawing.Point(835, 8);
+            this.btnCancel.Location = new System.Drawing.Point(1012, 8);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(80, 32);
+            this.btnCancel.Size = new System.Drawing.Size(80, 30);
             this.btnCancel.TabIndex = 1;
             this.btnCancel.Text = "取消";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -231,9 +235,9 @@
             // 
             this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnNext.Location = new System.Drawing.Point(721, 8);
+            this.btnNext.Location = new System.Drawing.Point(898, 8);
             this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(80, 32);
+            this.btnNext.Size = new System.Drawing.Size(80, 30);
             this.btnNext.TabIndex = 0;
             this.btnNext.Text = "下一步";
             this.btnNext.UseVisualStyleBackColor = true;
@@ -246,20 +250,93 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1178, 555);
+            this.panel1.Size = new System.Drawing.Size(1388, 636);
             this.panel1.TabIndex = 0;
+            // 
+            // tabControlGrading
+            // 
+            this.tabControlGrading.Controls.Add(this.tabPageBasin);
+            this.tabControlGrading.Controls.Add(this.tabPageBlock);
+            this.tabControlGrading.Controls.Add(this.tabPageTarget);
+            this.tabControlGrading.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlGrading.Location = new System.Drawing.Point(0, 0);
+            this.tabControlGrading.Multiline = true;
+            this.tabControlGrading.Name = "tabControlGrading";
+            this.tabControlGrading.SelectedIndex = 0;
+            this.tabControlGrading.Size = new System.Drawing.Size(1159, 579);
+            this.tabControlGrading.TabIndex = 0;
+            // 
+            // tabPageBasin
+            // 
+            this.tabPageBasin.Controls.Add(this.dgvBasin);
+            this.tabPageBasin.Location = new System.Drawing.Point(4, 22);
+            this.tabPageBasin.Name = "tabPageBasin";
+            this.tabPageBasin.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageBasin.Size = new System.Drawing.Size(1151, 553);
+            this.tabPageBasin.TabIndex = 0;
+            this.tabPageBasin.Text = "远景区";
+            this.tabPageBasin.UseVisualStyleBackColor = true;
+            // 
+            // tabPageBlock
+            // 
+            this.tabPageBlock.Controls.Add(this.dgvBlock);
+            this.tabPageBlock.Location = new System.Drawing.Point(4, 22);
+            this.tabPageBlock.Name = "tabPageBlock";
+            this.tabPageBlock.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageBlock.Size = new System.Drawing.Size(1151, 553);
+            this.tabPageBlock.TabIndex = 1;
+            this.tabPageBlock.Text = "有利区";
+            this.tabPageBlock.UseVisualStyleBackColor = true;
+            // 
+            // tabPageTarget
+            // 
+            this.tabPageTarget.Controls.Add(this.dataGridView1);
+            this.tabPageTarget.Location = new System.Drawing.Point(4, 22);
+            this.tabPageTarget.Name = "tabPageTarget";
+            this.tabPageTarget.Size = new System.Drawing.Size(1151, 553);
+            this.tabPageTarget.TabIndex = 2;
+            this.tabPageTarget.Text = "目标区";
+            this.tabPageTarget.UseVisualStyleBackColor = true;
+            // 
+            // dgvBasin
+            // 
+            this.dgvBasin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBasin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvBasin.Location = new System.Drawing.Point(3, 3);
+            this.dgvBasin.Name = "dgvBasin";
+            this.dgvBasin.Size = new System.Drawing.Size(1145, 547);
+            this.dgvBasin.TabIndex = 0;
+            // 
+            // dgvBlock
+            // 
+            this.dgvBlock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBlock.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvBlock.Location = new System.Drawing.Point(3, 3);
+            this.dgvBlock.Name = "dgvBlock";
+            this.dgvBlock.Size = new System.Drawing.Size(1145, 547);
+            this.dgvBlock.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1151, 553);
+            this.dataGridView1.TabIndex = 0;
             // 
             // GradingFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1184, 561);
+            this.ClientSize = new System.Drawing.Size(1394, 642);
             this.Controls.Add(this.panel1);
             this.MinimumSize = new System.Drawing.Size(1178, 555);
             this.Name = "GradingFrm";
             this.Padding = new System.Windows.Forms.Padding(3);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "区域分级";
+            //this.Load += new System.EventHandler(this.GradingFrm_Load);
             this.splitCtnerGraFrm.Panel1.ResumeLayout(false);
             this.splitCtnerGraFrm.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitCtnerGraFrm)).EndInit();
@@ -268,8 +345,14 @@
             this.splitContDataZone.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContDataZone)).EndInit();
             this.splitContDataZone.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel1.ResumeLayout(false);
+            this.tabControlGrading.ResumeLayout(false);
+            this.tabPageBasin.ResumeLayout(false);
+            this.tabPageBlock.ResumeLayout(false);
+            this.tabPageTarget.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBasin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBlock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -279,9 +362,15 @@
         private System.Windows.Forms.SplitContainer splitCtnerGraFrm;
         private System.Windows.Forms.TreeView treeViewGrad;
         private System.Windows.Forms.SplitContainer splitContDataZone;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button btnNext;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControlGrading;
+        private System.Windows.Forms.TabPage tabPageBasin;
+        private System.Windows.Forms.TabPage tabPageBlock;
+        private System.Windows.Forms.TabPage tabPageTarget;
+        private System.Windows.Forms.DataGridView dgvBasin;
+        private System.Windows.Forms.DataGridView dgvBlock;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Esofsga/EsofaUI/GradingFrm.cs
+++ Esofsga/EsofaUI/GradingFrm.cs
@@ -19,36 +19,62 @@ namespace EsofaUI
 
         private void btnNext_Click(object sender, EventArgs e)
         {
-            //核心区为当前选定区域
-           // if (rBtnCore.Checked == true)
-           // {
+           if (tabControlGrading.SelectedTab == tabPageBasin)
+            {
+                //////远景区为当前选定区域
+                //if (rBtnProspective.Checked == true)
+                //{
+                //    //创建远景区矩阵窗体实例
+                   ProspectAreaMatrixFrm prospectFrm = new ProspectAreaMatrixFrm();
+
+                //    //显示远景区矩阵窗体
+                   prospectFrm.Show();
+
+                //}
+            }
+
+            if (tabControlGrading.SelectedTab == tabPageBlock)
+            {
+                ////有利区为当前选定区域
+                //if (rBtnFavorable.Checked == true)
+                //{
+                //    //创建有利区矩阵窗体实例
+                    FavorableAreaMatrixFrm favorableFrm = new FavorableAreaMatrixFrm();
+
+                //    //显示有利区矩阵窗体
+                    favorableFrm.Show();
+                //}
+            }
+
+            if(tabControlGrading.SelectedTab == tabPageTarget)
+            {
+                //核心区为当前选定区域
+                // if (rBtnCore.Checked == true)
+                // {
                 //创建核心区矩阵窗体实例
                 CoreAreaMatrixFrm coreFrm = new CoreAreaMatrixFrm();
 
                 //显示核心区矩阵窗体
                 coreFrm.Show();
-           // }
+                // }
+            }
 
-            ////有利区为当前选定区域
-            //if (rBtnFavorable.Checked == true)
-            //{
-            //    //创建有利区矩阵窗体实例
-            //    FavorableAreaMatrixFrm favorableFrm = new FavorableAreaMatrixFrm();
-
-            //    //显示有利区矩阵窗体
-            //    favorableFrm.Show();
-            //}
-
-            //////远景区为当前选定区域
-            //if (rBtnProspective.Checked == true)
-            //{
-            //    //创建远景区矩阵窗体实例
-            //    ProspectAreaMatrixFrm prospectFrm = new ProspectAreaMatrixFrm();
-
-            //    //显示远景区矩阵窗体
-            //    prospectFrm.Show();
-
-            //}
+           
         }
+
+        //private void GradingFrm_Load(object sender, EventArgs e)
+        //{
+        //    this.treeViewGrad.CheckBoxes = true;
+        //    this.treeViewGrad.ShowLines = true;
+        //    this.treeViewGrad.DrawMode = System.Windows.Forms.TreeViewDrawMode.OwnerDrawAll;
+        //    this.treeViewGrad.DrawNode += new DrawTreeNodeEventHandler(treeViewGrad_DrawNode);
+        //}
+        //private void treeViewGrad_DrawNode(object sender, DrawTreeNodeEventArgs e)
+        //{
+        //    if(e.Node.Level == 0)
+        //    {
+                
+        //    }
+        //}
     }
 }
